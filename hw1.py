35b64b1cf864365f4a27116d634039a875075769
MyroslavaNY <MyroslavaNY@gmail.com>
2022-02-23 11:14:42 +0200

    part1

--- hw1.py
+++ hw1.py
@@ -8,4 +8,28 @@
 # 4) видалення юзера по id
 # 5) заміна статуса юзера на протилежний
 # 6) вихід з меню
+#
+# приклад юзера {'id':1,'name':'Max', 'age':35,'status':False}
+
 users_list = []
+
+
+def users(self, id, name, age, status):
+    self.id = id
+    self.name = name
+    self.age = age
+    self.status = status
+
+
+user1 = {1, 'Max', 35, False}
+user2 = {2, 'Diana', 30, True}
+
+users_list.append(user1)
+users_list.append(user2)
+print(users_list)
+
+# def userAge_key():
+#     return users.age
+# sort1 = sorted(users_list.age)
+# print(sort1)
+
